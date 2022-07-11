cbca9167d1e1d62d122453814d755e24fd497bd4
vishaluplers <vishal.s@uplers.in>
2022-07-11 11:22:20 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <img src="./logo192.png" width="40px" />
+          <h1>Fun facts about React</h1>
+          <ul>
+              <li>Was first released in 2013</li>
+              <li>Was originally created by Jordan Walke</li>
+              <li>Has well over 100K stars on GitHub</li>
+              <li>Is maintained by Facebook</li>
+              <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+      </div>
+  )
 }
-
-export default App;
+export default App
