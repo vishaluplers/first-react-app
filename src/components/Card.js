59891911948513d338d00b4d7d7d3edc2093ddce
vishaluplers <vishal.s@uplers.in>
2023-02-13 17:44:40 +0530

    nkn

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,7 @@
 import React from "react";
 export default function Card(props){
     return(
-<<<<<<< Updated upstream
    
-=======
-      
->>>>>>> Stashed changes
         <div className="card">
             <img src={props.link} alt="img"></img>
         </div>
